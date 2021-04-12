4c84acf988c230f827b704d4ffeb26d09ec85a82
Ajay <ajay.summer2@gmail.com>
2021-04-12 18:58:22 +0100

    Add onDelete event handler to delete tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,14 @@
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
   return (
     <div className="task">
-      <h3>{task.text}</h3>
+      <h3>
+        {task.text}
+        <span
+          className="fa fa-times"
+          onClick={() => onDelete(task.id)}
+          style={{ color: "red" }}>
+        </span>
+      </h3>
       <p>{task.day}</p>
     </div>
   )
