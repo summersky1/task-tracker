78535ffc3c72ff0b2887e7ff4cc145490786b54e
Ajay <ajay.summer2@gmail.com>
2021-04-12 18:33:35 +0100

    Add Tasks and Task components and display array of tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import Header from "./components/Header";
+import { useState } from 'react'
+import Header from "./components/Header"
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "task 1",
+      day: "Monday"
+    },
+    {
+      id: 2,
+      text: "task 2",
+      day: "Wednesday"
+    },
+    {
+      id: 3,
+      text: "task 3",
+      day: "Friday"
+    },
+  ])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
