28acbf56d8fbb06953fd5d7fc628a418bf5c0ded
Ajay <ajay.summer2@gmail.com>
2021-04-12 16:22:09 +0100

    Add basic Prop to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
   return (
     <header>
-      <h2>Task Tracker</h2>
+      <h2>{ title }</h2>
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: "Task Tracker"
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default Header
