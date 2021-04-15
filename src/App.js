73afe2e6d0c5902ccbf39dfa44f2d734fd68816c
Ajay <ajay.summer2@gmail.com>
2021-04-15 23:24:47 +0100

    Toggle reminder on json-server

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ function App() {
     return data
   }
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
   const toggleAdd = () => {
     setShowAddTask(!showAddTask)
   }
@@ -45,8 +51,21 @@ function App() {
     setTasks(tasks.filter(task => task.id !== id))
   }
 
-  const toggleReminder = id => {
-    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+    console.log(data)
+    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: data.reminder } : task))
   }
 
   return (
